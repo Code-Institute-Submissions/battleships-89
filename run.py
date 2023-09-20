0d94ee5dd7248ac32e41831c8ec6ee4357271a48
CyberCygnus <132933588+CyberCygnus@users.noreply.github.com>
2023-09-20 19:34:24 +0200

    Initialize board and set up emoji constants.

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Emoji constants
+WATER = "\U0001F7E6"  # Blue Square for water
+MISS = "\U0000274C"  # Red X for miss
+HIT = "\U0001F4A5"  # Explosion for hit
+SHIP = "\U0001F6A2"  # Ship for ship (this will not be shown on the computer board)
+
+# Initialize board
+def initialize_board(size):
+    return [[WATER for col in range(size)] for row in range(size)]
+
+# Main game loop
+def main():
+    size = 5
+    player_board = initialize_board(size)
+    computer_board = initialize_board(size)
+
+if __name__ == "__main__":
+    main()
