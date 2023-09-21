3963039e41820d3b725742a56a4c5433e353f6d9
CyberCygnus <132933588+CyberCygnus@users.noreply.github.com>
2023-09-21 15:44:24 +0200

    Add function to place random ships on the board and import random module.

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import random
+
 # Emoji constants
 WATER = "\U0001F7E6"  # Blue Square for water
 MISS = "\U0000274C"  # Red X for miss
@@ -7,6 +9,12 @@ SHIP = "\U0001F6A2"  # Ship for ship (this will not be shown on the computer boa
 # Initialize board
 def initialize_board(size):
     return [[WATER for col in range(size)] for row in range(size)]
+
+# Place random ships
+def place_ships(board, num_ships):
+    for i in range(num_ships):
+        row, col = random.randint(0, len(board) - 1), random.randint(0, len(board) - 1)
+        board[row][col] = SHIP
 
 # Main game loop
 def main():
