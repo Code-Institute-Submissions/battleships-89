c0bf9f78da00bd39bb42d07dc10114890c6239a8
CyberCygnus <132933588+CyberCygnus@users.noreply.github.com>
2023-09-22 19:46:11 +0200

    Refactored the Battleship game code to comply with PEP8 standards.
    - Broke down longer lines for improved readability.
    - Introduced emoji symbols for a more visual game experience.
    - Streamlined game logic for more robust gameplay.

--- run.py
+++ run.py
@@ -4,53 +4,35 @@ import random
 WATER = "\U0001F7E6"  # Blue Square for water
 MISS = "\U0000274C"  # Red X for miss
 HIT = "\U0001F4A5"  # Explosion for hit
-SHIP = "\U0001F6A2"  # Ship for ship (this will not be shown on the computer board)
+SHIP = "\U0001F6A2"  # Ship for ship
 
-# Initialize board
+
 def initialize_board(size):
     return [[WATER for col in range(size)] for row in range(size)]
 
-# Place random ships
+
 def place_ships(board, num_ships):
-    """
-    Randomly place a specified number of ships on the provided game board.
-    Args:
-    - board (list of list): 2D list representing the game board.
-    - num_ships (int): Number of ships to be placed on the board.
-    """
     placed = 0
     attempts = 0
     while placed < num_ships:
-        row, col = random.randint(0, len(board) - 1), random.randint(0, len(board) - 1)
-        if board[row][col] != SHIP:  # Ensure the cell isn't already occupied by a ship
+        row = random.randint(0, len(board) - 1)
+        col = random.randint(0, len(board) - 1)
+        if board[row][col] != SHIP:
             board[row][col] = SHIP
             placed += 1
         attempts += 1
-        if attempts > 1000:  # Safeguard against infinite loop
-            raise ValueError("Too many attempts to place ships. Possibly too many ships for the board size.")
+        if attempts > 1000:
+            raise ValueError("Too many attempts to place ships."
+                             " Possibly too many ships for the board size.")
 
-# Convert user input to coordinates
+
 def input_to_coordinates(move):
-    """
-    Convert the user's textual input into corresponding row and column indices on the game board.
-    Args: move (str): The user's input in the format of a letter followed by a number (e.g., 'A1', 'B3').
-    Returns tuple: A tuple containing the row and column indices corresponding to the user's input.
-    Note: input validation!
-    """
     row = ord(move[0].upper()) - ord('A')
     col = int(move[1]) - 1
     return row, col
 
-# Make a move
+
 def make_move(board, row, col):
-    """
-    Records a move on the provided board at the given row and column.
-    Args:
-    - board (list of lists): The 2D game board where the move will be recorded.
-    - row (int): The zero-based row index where the move will be made.
-    - col (int): The zero-based column index where the move will be made.
-    Returns str: "hit" if the move results in a hit, "miss" if it's a miss, "already targeted" if cell was already hit/miss.
-    """
     if board[row][col] == MISS or board[row][col] == HIT:
         return "already targeted"
     elif board[row][col] == SHIP:
@@ -60,42 +42,57 @@ def make_move(board, row, col):
         board[row][col] = MISS
         return "miss"
 
+
+def convert_cell(cell):
+    return cell if cell != SHIP else WATER
+
+
 def display_boards(player_board, computer_board):
-    # Display computer's board without showing ships
     print("\nComputer's Board:")
-        # Display rows with row labels (A, B, C, etc.)
     for idx, row in enumerate(computer_board):
         label = chr(idx + ord('A'))
-        print(f"{label} |", " ".join([cell if cell != SHIP else WATER for cell in row]))
+        row_content = " ".join([convert_cell(cell) for cell in row])
+        print(f"{label} |", row_content)
 
-    # Display column headings between the two boards
-    print("  ".join(['   '] + ['1', '2', '3', '4', '5']))  # Adjust if you change the board size
-    # Display player's board with all information visible and with row labels
+    column_headers = ['   ', '1', '2', '3', '4', '5']
+    print("  ".join(column_headers))
+
     for idx, row in enumerate(player_board):
         label = chr(idx + ord('A'))
         print(f"{label} |", " ".join(row))
 
+
 def count_hits(board):
-    """
-    Counts the number of HIT emojis on the given board.
-    Args:
-    - board (list of lists): The 2D game board.
-    Returns int: The number of hits on the board.
-    """
     return sum(row.count(HIT) for row in board)
+
 
 def get_valid_move():
     while True:
         move = input("Enter your move (e.g., 'A1'), 'x' to exit: ")
         if move.lower() == 'x':
             print("Thanks for playing! Exiting the game.")
-            exit()  # Exit the game
-        elif len(move) == 2 and move[0].upper() in 'ABCDE' and move[1] in '12345':  # Adjust if board size changes
-            return move.upper()  # Return standardized input
+            exit()
+        valid_row = len(move) == 2 and move[0].upper() in 'ABCDE'
+        valid_col = move[1] in '12345'
+        if valid_row and valid_col:
+            return move.upper()
         else:
-            print("Invalid input. Please provide coordinates in the format 'A1', where 'A' is the row and '1' is the column.")
+            print("Invalid input. Use the format 'A1'")
 
-# Main game loop
+
+def computer_turn(player_board, size):
+    comp_row = random.randint(0, size - 1)
+    comp_col = random.randint(0, size - 1)
+    comp_result = make_move(player_board, comp_row, comp_col)
+
+    while comp_result == "already targeted":
+        comp_row = random.randint(0, size - 1)
+        comp_col = random.randint(0, size - 1)
+        comp_result = make_move(player_board, comp_row, comp_col)
+
+    return comp_result
+
+
 def main():
     size = 5
     num_ships = 5
@@ -108,7 +105,6 @@ def main():
 
     while True:
         display_boards(player_board, computer_board)
-        
         move = get_valid_move()
         row, col = input_to_coordinates(move)
         result = make_move(computer_board, row, col)
@@ -124,20 +120,12 @@ def main():
         elif result == "miss":
             print("Miss! ❌")
 
-        # Computer's turn
-        comp_row, comp_col = random.randint(0, size - 1), random.randint(0, size - 1)
-        comp_result = make_move(player_board, comp_row, comp_col)
-        
-        while comp_result == "already targeted":
-            comp_row, comp_col = random.randint(0, size - 1), random.randint(0, size - 1)
-            comp_result = make_move(player_board, comp_row, comp_col)
-        
+        comp_result = computer_turn(player_board, size)
         if comp_result == "hit":
             print("Computer hit your ship!")
         elif comp_result == "miss":
             print("Computer missed!")
 
-        # Check for end game conditions
         if count_hits(computer_board) == num_ships:
             print("Congratulations! You sunk all the computer's ships!")
             break
@@ -145,5 +133,7 @@ def main():
             print("All your ships have been sunk! Game Over!")
             break
 
+
 if __name__ == "__main__":
     main()
+
